0befcd2efb54baefcc654c239f6f291f3be2035c
Filo-x <crystalreneecole@gmail.com>
2024-10-17 07:14:31 -0500

    Update vite.config.ts
    added base: '/filo-x/github.io/'

--- my-quiz-app/vite.config.ts
+++ my-quiz-app/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 
+ base: '/filo-x/github.io/'
 export default defineConfig({
   plugins: [react()],
 })
